2ceb87c1fe1ad7304bf20eed17153b7c27fee24e
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2025-07-09 15:32:27 +0300

    feat: allow users to exit app using an 'exit' command

--- tasks.js
+++ tasks.js
@@ -35,7 +35,7 @@ function startApp(name){
  */
 function onDataReceived(text) {
   text = text.trim();
-  if (text === "quit") {
+  if (text === "quit" || text === "exit") {
     quit();
   }
   else if(text === "hello"){
